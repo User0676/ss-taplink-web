13ec514014e0b3b20f8c1616d552af13d1edc5e7
Ali Zhumatayev <alizhumataeyev@Alis-MacBook-Air-2.local>
2024-07-05 19:50:50 +0500

    searchFunc

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,25 +1,40 @@
-import React, {useContext} from 'react';
+import React, { useContext, useState } from 'react';
 import styles from './SearchBar.module.scss';
 import { TextField } from '@mui/material';
 import Button from "@mui/material/Button";
 import FilterListIcon from "@mui/icons-material/Tune";
-import {CartContext} from "../../contexts/CartContext";
-
+import { CartContext } from "../../contexts/CartContext";
 
 const SearchBar = () => {
+    const { openFilterModal } = useContext(CartContext);
+    const [searchQuery, setSearchQuery] = useState('');
 
-        const {openFilterModal, openSortModal, sortType} = useContext(CartContext);
+    const handleSearchChange = (event) => {
+        const query = event.target.value;
+        setSearchQuery(query);
+        console.log(query); // выводим значение в терминал
+    };
 
+    const handleKeyPress = (event) => {
+        if (event.key === 'Enter') {
+            console.log('Enter pressed:', searchQuery); // отправляем значение при нажатии Enter
+        }
+    };
 
-        return (
-            <div className={styles.searchWithFilter}>
-                <div className={styles.searchBar}>
-                    <TextField fullWidth placeholder="Поиск в магазине"/>
-
-                </div>
-                <Button onClick={openFilterModal}><FilterListIcon/></Button>
+    return (
+        <div className={styles.searchWithFilter}>
+            <div className={styles.searchBar}>
+                <TextField
+                    fullWidth
+                    placeholder="Поиск в магазине"
+                    value={searchQuery}
+                    onChange={handleSearchChange}
+                    onKeyPress={handleKeyPress} // добавляем обработчик нажатия клавиши
+                />
             </div>
-        )
+            <Button onClick={openFilterModal}><FilterListIcon /></Button>
+        </div>
+    );
 };
 
 export default SearchBar;
